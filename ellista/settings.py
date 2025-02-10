b4c03d31e64c54fc163d2156df98d19c6130669e
AkhlaqAltaf <108485854+AkhlaqAltaf@users.noreply.github.com>
2025-02-10 09:50:39 +0500

    Update settings.py

--- ellista/settings.py
+++ ellista/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-ne3yc=yh6pqg!2h8s0$&6bpy^w(aehv%o$3yj$!_b(_#!gik8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -129,8 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/root/ellista/_static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/root/ellista/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
